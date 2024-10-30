7801fcd18cffeb7df6b73ac67565534b61af9c8e
Kamil Handzlik <kamol0209@gmail.com>
2024-10-30 19:09:15 +0100

    Created register, login and home views debbued access to static files.

--- WebProject/register/urls.py
+++ WebProject/register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login'),
 ]
