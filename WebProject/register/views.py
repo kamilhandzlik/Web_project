8d5357acd9228aed054a1aa13f014d5a7acb16d2
Kamil Handzlik <kamol0209@gmail.com>
2024-10-30 19:41:57 +0100

    Changed stuff vith registration

--- WebProject/register/views.py
+++ WebProject/register/views.py
@@ -12,6 +12,48 @@ from .models import *
 # Create your views here.
 
 def RegisterView(request):
+
+    if request.method =="POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # password2 = request.POST.get('password2')
+
+        user_data_has_error = False
+
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, 'Taka nazwa użytkownika jest zajęta.')
+
+        # if User.object.filter(email=email).exists():
+        #     user_data_has_error = True
+        #     messages.error(request, 'Ten email jest zajęty.')
+
+        if len(password) < 8:
+            user_data_has_error = True
+            messages.error(request, 'Twoje hasło jest zakrótkie.Minimum 8 znaków')
+
+        # if password2 != password1:
+        #     user_data_has_error = True
+        #     messages.error(request, 'Hasła nie pasują do siebie.')
+
+        if user_data_has_error:
+            return redirect('register')
+        else:
+            new_user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password1=password,
+                # password2=password2
+            )
+            messages.success(request, 'Konto zostało utworzone poprawnie. Teraz się zaloguj.')
+            return render(request, 'login.html')
+
+
     return  render(request, 'register.html')
 
 def LoginView(request):
