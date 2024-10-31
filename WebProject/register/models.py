352223dc5ba34530c79939760bed6710eab73b6f
Kamil Handzlik <kamol0209@gmail.com>
2024-10-31 10:14:05 +0100

    created resetpassword model and made migrations

--- WebProject/register/models.py
+++ WebProject/register/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class PasswordReset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reset_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    create_when = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Resetowanie hasła dla użytkownika {self.user.username} w dniu {self.create_when}"
